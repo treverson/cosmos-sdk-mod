2d7c8a0b1d258e5c9f4ef2d8ea9044921bf34be6
vaibhavchellani <vaibhavchellani223@gmail.com>
2018-08-11 08:12:39 +0530

    added handler

--- x/sideBlock/handler.go
+++ x/sideBlock/handler.go
@@ -8,48 +8,18 @@ func NewHandler(k Keeper) sdk.Handler {
 	return func(ctx sdk.Context, msg sdk.Msg) sdk.Result {
 		// NOTE msg already has validate basic run
 		switch msg := msg.(type) {
-		case MsgUnrevoke:
-			return handleMsgUnrevoke(ctx, msg, k)
+		case MsgSideBlock:
+			return handleMsgSideBlock(ctx, msg, k)
 		default:
-			return sdk.ErrTxDecode("invalid message parse in staking module").Result()
+			return sdk.ErrTxDecode("Invalid message in side block module ").Result()
 		}
 	}
 }
-func handleMsgUnrevoke(ctx sdk.Context, msg MsgUnrevoke, k Keeper) sdk.Result {
 
-	// Validator must exist
-	validator := k.validatorSet.Validator(ctx, msg.ValidatorAddr)
-	if validator == nil {
-		return ErrNoValidatorForAddress(k.codespace).Result()
-	}
+func handleMsgSideBlock(ctx sdk.Context, msg MsgSideBlock, k Keeper) sdk.Result {
 
-	addr := validator.GetPubKey().Address()
-
-	// Signing info must exist
-	info, found := k.getValidatorSigningInfo(ctx, addr)
-	if !found {
-		return ErrNoValidatorForAddress(k.codespace).Result()
-	}
-
-	// Cannot be unrevoked until out of jail
-	if ctx.BlockHeader().Time < info.JailedUntil {
-		return ErrValidatorJailed(k.codespace).Result()
-	}
-
-	if ctx.IsCheckTx() {
-		return sdk.Result{}
-	}
-
-	// Update the starting height (so the validator can't be immediately revoked again)
-	info.StartHeight = ctx.BlockHeight()
-	k.setValidatorSigningInfo(ctx, addr, info)
-
-	// Unrevoke the validator
-	k.validatorSet.Unrevoke(ctx, validator.GetPubKey())
-
-	tags := sdk.NewTags("action", []byte("unrevoke"), "validator", msg.ValidatorAddr.Bytes())
 
 	return sdk.Result{
-		Tags: tags,
+
 	}
 }
